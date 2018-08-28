be6906287e63fd578c864bf9124793e602a455c5
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2018-08-28 18:06:30 +0500

    Buffer commands switched to false.

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const initialize = () => {
-    mongoose.connect('mongodb://localhost/testdb', { useNewUrlParser: true }); 
+    mongoose.connect('mongodb://localhost/testdb', { useNewUrlParser: true, bufferCommands: false }); 
 };
 
 export default initialize;
