97d2d5a32dff017c4ee69abbaee17c75ee4be04d
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2018-12-07 15:32:59 +0500

    Add integration tests for mongoose crud model

--- src/model/MongooseCrudModel.int.test.js
+++ src/model/MongooseCrudModel.int.test.js
@@ -1,8 +1,102 @@
-describe("Mongoose crud model tests", () => {
-    
-    describe('Filtering', () => {
+import mongoose, { Schema } from "mongoose";
+import { expect } from "chai";
+import MongooseCrudModel from "./MongooseCrudModel";
 
-        /*it('Date range', async () => {
+mongoose.set("debug", true);
+
+describe("Mongoose crud model tests", () => {
+
+    const subdocumentSchema = new Schema({
+
+    });
+
+    const arraySchema = new Schema({
+        
+    });
+
+    const entrySchema = new Schema({
+        counter: Number,
+        date: Date,
+
+    });
+    
+    let Entry, connection, model;
+
+    before(async () => {
+        connection = await mongoose.createConnection(process.env.DB_URL, { useNewUrlParser: true });
+        Entry = connection.model("Entry", entrySchema);
+        model = new MongooseCrudModel(Entry);
+    });
+
+    after(async () => {
+        await connection.dropDatabase();
+        await connection.close(true);
+    });
+
+    const cleanDatabase = async () => await Entry.remove({ });
+
+    beforeEach(cleanDatabase);
+
+    const populateDatabase = async entryCount => {
+        let date = new Date();
+        for (let counter = 0; counter < entryCount; counter++) {
+            date = new Date(date);
+            date.setDate(date.getDate() + 1);
+            await new Entry({ counter, date }).save();
+        }
+    };
+
+    describe("GetAll", () => {
+
+        it("Empty page", async () => {
+            const result = await model.getAll({ page: 0, size: 20 });
+            expect(result).to.deep.equal([ ]);
+        });
+
+        it("Get page 2 with size 5 and count 12", async () => {
+            await populateDatabase(12);
+            const result = await model.getAll({ page: 1, size: 5 });
+            expect(result.length).to.equal(5);
+        });
+
+        it("Get page 3 with size 5 and count 12", async () => {
+            await populateDatabase(12);
+            const result = await model.getAll({ page: 2, size: 5 });
+            expect(result.length).to.equal(2);
+        });
+
+        it("Get page 1 with size 5 and count 12 with wide number filter and sort by counter asc", async () => {
+            await populateDatabase(12);
+            const result = await model.getAll({ page: 0, size: 5, filter: { counter: { $gte: 2 } }, sort: { counter: 1 } });
+            expect(result.length).to.equal(5);
+            expect(result[0].counter).to.equal(2);
+        });
+
+        it("Get page 1 with size 5 and count 12 with wide number filter and sort by counter desc", async () => {
+            await populateDatabase(12);
+            const result = await model.getAll({ page: 0, size: 5, filter: { counter: { $gte: 2 } }, sort: { counter: -1 } });
+            expect(result.length).to.equal(5);
+            expect(result[0].counter).to.equal(11);
+        });
+
+        it("Get page 1 with size 5 and count 12 with narrow number filter", async () => {
+            await populateDatabase(12);
+            const result = await model.getAll({ page: 0, size: 5, filter: { counter: 2 } });
+            expect(result.length).to.equal(1);
+        });
+
+        it("Get page 1 with size 5 and count 12 with permission read filter and regular filter to restricted entry", async () => {
+            await populateDatabase(12);
+            const permissions = { "readFilter": { counter: 2 } };
+            const result = await model.getAll({ page: 0, size: 5, filter: { counter: 5 } }, permissions);
+            expect(result.length).to.equal(0);
+        });
+
+    });
+
+    /*describe('Filtering', () => {
+
+        it('Date range', async () => {
             const startDate = new Date(now);
             const endDate = new Date(now);
             startDate.setDate(startDate.getDate() + 5);
@@ -34,11 +128,11 @@ describe("Mongoose crud model tests", () => {
                     .query(qs.stringify({ filter: { $or: [ { number: 12 }, { email: "mail1@mail.com" } ] } }))
                     .expect(200).send();
             expect(res.get("X-Total-Count")).to.equal("2");
-        });*/
+        });
 
     });
 
-    /*describe('Sorting', () => {
+    describe('Sorting', () => {
 
         it ('Desc number', async () => {
             const res = await request(server).get("/users")
--- src/model/MongooseCrudModel.js
+++ src/model/MongooseCrudModel.js
@@ -1,21 +1,40 @@
+import { flatten } from "flat";
+
 class MongooseCrudModel {
 
     constructor(Model) {
         this.Model = Model;
     }
 
-    async getAll({ page, size, filter, sort }, permissions) {
-        if (filter.id) filter._id = filter.id;
+    async getAll({ page, size, filter, sort }, permissions = { }) {
+
+        const { readFilter } = permissions;
         const getAllQuery = this.Model.find()
             .skip(page * size)
             .limit(size);
-        if (filter) getAllQuery.where(filter);
+        const filterArray = [];
+        if (readFilter) filterArray.push(readFilter);
+        if (filter) filterArray.push(filter);
+
+        let resultFilter;
+        switch(filterArray.length) {
+            case 1:
+                resultFilter = filterArray[0]
+                break;
+            case 2:
+                resultFilter = { $and: filterArray };
+                break;
+            default:
+        }
+
+        if (resultFilter) getAllQuery.where(resultFilter);
+
         if (sort) getAllQuery.sort(sort);
         return await getAllQuery.exec();
+
     }
 
     async count(filter, permissions) {
-        if (filter.id) filter._id = filter.id;
         const query = this.Model.count();
         if (filter) query.where(filter);
         return await query.exec();
@@ -27,17 +46,30 @@ class MongooseCrudModel {
     }
 
     async getOne(filter, permissions) {
-        if (filter.id) filter._id = filter.id;
+        if (filter && filter.id) filter._id = filter.id;
         return await this.Model.findOne(filter);
     }
 
     async updateOne(filter, payload, permissions) {
-        if (filter.id) filter._id = filter.id;
-        return await this.Model.findOneAndUpdate(filter, payload, { runValidators: true, context: 'query' });
+
+        if (filter && filter.id) filter._id = filter.id;
+        const { modifyFields } = permissions;
+
+        let $set;
+        if (modifyFields) {
+            const flattened = flatten(payload);
+            $set = Object.keys(flattened)
+                .reduce((prev, property) => !modifyFields[property] ? prev : { ...prev, [property]: flattened.property }, { });
+        } else {
+            $set = payload;
+        }
+
+        return await this.Model.findOneAndUpdate(filter, { $set }, { runValidators: true, context: 'query' });
+
     }
 
     async deleteOne(filter, permissions) {
-        if (filter.id) filter._id = filter.id;
+        if (filter && filter.id) filter._id = filter.id;
         return await this.Model.findOneAndDelete(filter);
     }
 
