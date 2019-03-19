368147b2e2706d65200f28e076ceb09381a199b7
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-03-19 09:51:02 +0500

    Fix recursive path parsing

--- src/plugins/deepFind.js
+++ src/plugins/deepFind.js
@@ -14,18 +14,16 @@ const shouldIncludePath = (path, projection) => {
 function reduceSchemaRecursive(schema, reducer, initialValue, context = {}) {
   const { paths = [], maxDepth, projection, parentArrays = [] } = context;
   return Object.keys(schema.paths).reduce((accumulator, path) => {
-    let { options } = schema.paths[path];
-    const type = options[0] || options;
-    //options = type.constructor.name === 'Schema' ? type.
-    const isArray = Array.isArray(options.type);
+    const currentSchemaPath = schema.paths[path];
+    const options = ( currentSchemaPath.caster && currentSchemaPath.caster.options ) || currentSchemaPath.options;
     const currentPaths = [...paths, path];
     const currentPathsString = currentPaths.join('.');
     const nextDepth = maxDepth === true ? maxDepth : maxDepth - 1;
-    const nextParentArrays = isArray ? [...parentArrays, path] : parentArrays;
+    const nextParentArrays = currentSchemaPath.instance === 'Array' ? [...parentArrays, path] : parentArrays;
     const includePath = shouldIncludePath(currentPathsString, projection);
 
-    let currentAccValue = ( includePath && reducer(accumulator, currentPathsString, options, parentArrays) ) || accumulator;
-    if ((options.ref) && ( nextDepth === true || nextDepth >= 0 ) && includePath) {
+    let currentAccValue = ( includePath && reducer(accumulator, currentPathsString, currentSchemaPath, options, parentArrays) ) || accumulator;
+    if (options.ref && ( nextDepth === true || nextDepth >= 0 ) && includePath) {
       const targetModel = this.db.model(options.ref);
       currentAccValue = reduceSchemaRecursive.call(
         this,
@@ -34,7 +32,15 @@ function reduceSchemaRecursive(schema, reducer, initialValue, context = {}) {
         currentAccValue,
         { paths: currentPaths, maxDepth: nextDepth, projection, parentArrays: nextParentArrays }
       );
-    }
+    } else if (currentSchemaPath.schema) {
+      currentAccValue = reduceSchemaRecursive.call(
+        this,
+        currentSchemaPath.schema,
+        reducer,
+        currentAccValue,
+        { paths: currentPaths, maxDepth: nextDepth, projection, parentArrays: nextParentArrays }
+      );
+    } 
     return currentAccValue;
   }, initialValue);
 }
@@ -80,10 +86,10 @@ export function deepFindAll(options = {}) {
   else if (this.schema.options.maxDepth || this.schema.options.maxDepth === 0) maxDepth = this.schema.options.maxDepth;
   else maxDepth = 1;
 
-  const pathsTree = reduceSchemaRecursive.call(this, this.schema, (accumulator, property, schema, parentArrays) => {
+  const pathsTree = reduceSchemaRecursive.call(this, this.schema, (accumulator, property, schema, options, parentArrays) => {
     let type;
-    if (schema.ref) type = 'ref';
-    else if (Array.isArray(schema)) type = 'array';
+    if (options.ref) type = 'ref';
+    else if (schema.instance === 'Array') type = 'array';
     else type = 'path';
     return [...accumulator, { property, type, parentArrays }];
   }, [], { maxDepth, projection });
