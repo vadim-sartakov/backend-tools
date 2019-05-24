4e1d723508690dcdf30747d7681d0d91dc238994
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-05-24 16:10:08 +0500

    Update exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export * from "./utils";
 export * from "./middleware";
+export { default as graphFindPlugin } from "./plugins/graphFind";
 export { default as CrudRouter } from "./router/crud";
 export { default as MongooseCrudModel } from "./model/MongooseCrudModel";
-export { default as SequelizeCrudModel } from "./model/SequelizeCrudModel";
